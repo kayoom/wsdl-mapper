4ffe3975a83df93523c3f4beeda3ce149252eaed
Marian Theisen <marian.theisen@kayoom.com>
2015-08-26 10:01:30 +0200

    refactored tests to avoid namespace polution

--- test/wsdl_mapper/schema/xsd_file_test.rb
+++ test/wsdl_mapper/schema/xsd_file_test.rb
@@ -2,18 +2,22 @@ require 'test_helper'
 
 require 'wsdl_mapper/schema/xsd_file'
 
-class XsdFileTest < Minitest::Test
-  def setup
-    @file = WsdlMapper::Schema::XsdFile.new(TestHelper.get_fixture("w3_example.xsd"))
-  end
+module SchemaTests
+  class XsdFileTest < Minitest::Test
+    include WsdlMapper::Schema
 
-  def test_that_it_can_load_a_simple_xsd_file
-    assert_kind_of WSDL::XMLSchema::Schema, @file.root
-  end
+    def setup
+      @file = XsdFile.new(TestHelper.get_fixture("w3_example.xsd"))
+    end
 
-  def test_that_it_found_the_embedded_schema
-    element_names = @file.root.collect_elements.map { |e| e.name.name }
+    def test_that_it_can_load_a_simple_xsd_file
+      assert_kind_of WSDL::XMLSchema::Schema, @file.root
+    end
 
-    assert_includes element_names, "note"
+    def test_that_it_found_the_embedded_schema
+      element_names = @file.root.collect_elements.map { |e| e.name.name }
+
+      assert_includes element_names, "note"
+    end
   end
 end
