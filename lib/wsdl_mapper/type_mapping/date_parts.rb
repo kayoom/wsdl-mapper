ba7c10b4c58dc6bf04ec2c09f058d77bc0048121
Marian Theisen <marian.theisen@kayoom.com>
2015-10-22 16:29:17 +0200

    date parts type mappings

---
+++ lib/wsdl_mapper/type_mapping/date_parts.rb
@@ -0,0 +1,105 @@
+require 'wsdl_mapper/type_mapping/base'
+require 'wsdl_mapper/dom/builtin_type'
+
+require 'wsdl_mapper/core_ext/time_duration'
+
+module WsdlMapper
+  module TypeMapping
+    module DateParts
+      class Base < WsdlMapper::TypeMapping::Base
+      end
+
+      Day = Base.new do
+        register_ruby_types [
+          WsdlMapper::CoreExt::TimeDuration
+        ]
+
+        register_xml_types %w[
+          gDay
+        ]
+
+        def to_ruby string
+          WsdlMapper::CoreExt::TimeDuration.new days: string.to_s.to_i
+        end
+
+        def to_xml object
+          "%02d" % object.days
+        end
+
+        Month = Base.new do
+          register_ruby_types [
+            WsdlMapper::CoreExt::TimeDuration
+          ]
+
+          register_xml_types %w[
+            gMonth
+          ]
+
+          def to_ruby string
+            WsdlMapper::CoreExt::TimeDuration.new months: string.to_s.to_i
+          end
+
+          def to_xml object
+            "%02d" % object.months
+          end
+        end
+
+        Year = Base.new do
+          register_ruby_types [
+              WsdlMapper::CoreExt::TimeDuration
+            ]
+
+          register_xml_types %w[
+            gYear
+          ]
+
+          def to_ruby string
+            WsdlMapper::CoreExt::TimeDuration.new years: string.to_s.to_i
+          end
+
+          def to_xml object
+            "%04d" % object.years
+          end
+        end
+
+        YearMonth = Base.new do
+          register_ruby_types [
+              WsdlMapper::CoreExt::TimeDuration
+            ]
+
+          register_xml_types %w[
+            gYearMonth
+          ]
+
+          def to_ruby string
+            years, months = string.split '-'
+            WsdlMapper::CoreExt::TimeDuration.new years: years.to_i, months: months.to_i
+          end
+
+          def to_xml object
+            "%04d-%02d" % [object.years, object.months]
+          end
+        end
+
+        MonthDay = Base.new do
+          register_ruby_types [
+              WsdlMapper::CoreExt::TimeDuration
+            ]
+
+          register_xml_types %w[
+            gMonthDay
+          ]
+
+          def to_ruby string
+            months, days = string.split '-'
+            WsdlMapper::CoreExt::TimeDuration.new months: months.to_i, days: days.to_i
+          end
+
+          def to_xml object
+            "%02d-%02d" % [object.months, object.days]
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/wsdl_mapper/type_mapping/date_parts_test.rb
@@ -0,0 +1,72 @@
+require 'test_helper'
+
+require 'wsdl_mapper/type_mapping/date_parts'
+
+module TypeMappingTests
+  module DatePartsTest
+    class DayTest < ::Minitest::Test
+      include WsdlMapper::TypeMapping::DateParts
+      include WsdlMapper::CoreExt
+
+      def test_to_ruby
+        assert_equal TimeDuration.new(days: 14), Day.to_ruby("14")
+      end
+
+      def test_to_xml
+        assert_equal "04", Day.to_xml(TimeDuration.new(days: 4))
+      end
+    end
+
+    class MonthTest < ::Minitest::Test
+      include WsdlMapper::TypeMapping::DateParts
+      include WsdlMapper::CoreExt
+
+      def test_to_ruby
+        assert_equal TimeDuration.new(months: 11), Month.to_ruby("11")
+      end
+
+      def test_to_xml
+        assert_equal "04", Month.to_xml(TimeDuration.new(months: 4))
+      end
+    end
+
+    class YearTest < ::Minitest::Test
+      include WsdlMapper::TypeMapping::DateParts
+      include WsdlMapper::CoreExt
+
+      def test_to_ruby
+        assert_equal TimeDuration.new(years: 11), Year.to_ruby("0011")
+      end
+
+      def test_to_xml
+        assert_equal "0004", Year.to_xml(TimeDuration.new(years: 4))
+      end
+    end
+
+    class YearMonthTest < ::Minitest::Test
+      include WsdlMapper::TypeMapping::DateParts
+      include WsdlMapper::CoreExt
+
+      def test_to_ruby
+        assert_equal TimeDuration.new(years: 11, months: 6), YearMonth.to_ruby("0011-06")
+      end
+
+      def test_to_xml
+        assert_equal "0004-05", YearMonth.to_xml(TimeDuration.new(years: 4, months: 5))
+      end
+    end
+
+    class MonthDayTest < ::Minitest::Test
+      include WsdlMapper::TypeMapping::DateParts
+      include WsdlMapper::CoreExt
+
+      def test_to_ruby
+        assert_equal TimeDuration.new(months: 6, days: 9), MonthDay.to_ruby("06-09")
+      end
+
+      def test_to_xml
+        assert_equal "06-09", MonthDay.to_xml(TimeDuration.new(months: 6, days: 9))
+      end
+    end
+  end
+end
