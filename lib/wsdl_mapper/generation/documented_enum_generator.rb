1b9698886be41c3844d74239fd910eff73467e55
Marian Theisen <marian.theisen@kayoom.com>
2015-10-29 10:38:09 +0100

    removed comment

--- lib/wsdl_mapper/generation/documented_enum_generator.rb
+++ lib/wsdl_mapper/generation/documented_enum_generator.rb
@@ -5,26 +5,6 @@ module WsdlMapper
   module Generation
     class DocumentedEnumGenerator < DefaultEnumGenerator
 
-      # def generate ttg, result
-      #   file_name = @generator.context.path_for ttg.name
-      #
-      #   modules = ttg.name.parents.reverse
-      #
-      #   File.open file_name, 'w' do |io|
-      #     f = @generator.get_formatter io
-      #     values_to_generate = get_values_to_generate(ttg)
-      #
-      #     open_modules f, modules
-      #     open_class f, ttg
-      #     generate_constant_assignments f, values_to_generate
-      #     generate_values_array f, values_to_generate
-      #     close_class f, ttg
-      #     close_modules f, modules
-      #   end
-      #   result.files << file_name
-      #   self
-      # end
-
       protected
       def generate_constant_assignments f, values_to_generate
         yard = YardDocFormatter.new f
