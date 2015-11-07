10b1b9dcbfc52ec1e1b74ae9852217e931308e3b
Marian Theisen <marian.theisen@kayoom.com>
2015-11-07 14:34:42 +0100

    some documentation for default namer

--- lib/wsdl_mapper/naming/default_namer.rb
+++ lib/wsdl_mapper/naming/default_namer.rb
@@ -6,44 +6,69 @@ require 'wsdl_mapper/naming/abstract_namer'
 
 module WsdlMapper
   module Naming
+    # This is the default Namer implementation. It provides the de-facto standard of organizing and naming
+    # classes in ruby:
+    #
+    # 1. Class/Module names are CamelCase (e.g. `SomeType`)
+    # 2. File names are under_score (e.g. `some_type.rb`)
+    # 3. Each class in its own file
+    # 4. (De)Serializers are put within the same module as XSD Types
     class DefaultNamer < AbstractNamer
       include Inflector
 
+      # Initializes a new {DefaultNamer} instance.
+      #
+      # @param [Array<String>] module_path the root module for the generated classes, e.g. `['MyApi', 'Types']` => `MyApi::Types::SomeClass` in `my_api/types/some_class.rb`
+      # @param [String] content_attribute_name the accessor name for {file:concepts/wrapping_types.md wrapping types} (complex type with simple content and simple types with restrictions)
       def initialize module_path: [], content_attribute_name: 'content'
         @module_path = module_path
         @content_attribute_name = content_attribute_name
       end
 
+      # @param [WsdlMapper::Dom::ComplexType, WsdlMapper::Dom::SimpleType] type
+      # @return [TypeName]
       def get_type_name type
         type_name = TypeName.new get_class_name(type), get_class_module_path(type), get_class_file_name(type), get_class_file_path(type)
         type_name.parent = make_parents @module_path
         type_name
       end
 
+      # @param [WsdlMapper::Dom::ComplexType, WsdlMapper::Dom::SimpleType] type
+      # @return [TypeName]
       def get_s8r_name type
         type_name = TypeName.new get_s8r_class_name(type), get_s8r_module_path(type), get_s8r_file_name(type), get_s8r_file_path(type)
         type_name.parent = make_parents @module_path
         type_name
       end
 
+      # @param [WsdlMapper::Dom::ComplexType, WsdlMapper::Dom::SimpleType] type
+      # @return [TypeName]
       def get_d10r_name type
         type_name = TypeName.new get_d10r_class_name(type), get_d10r_module_path(type), get_d10r_file_name(type), get_d10r_file_path(type)
         type_name.parent = make_parents @module_path
         type_name
       end
 
+      # @param [WsdlMapper::Dom::Property] property
+      # @return [PropertyName]
       def get_property_name property
         PropertyName.new get_accessor_name(property.name.name), get_var_name(property.name.name)
       end
 
+      # @param [WsdlMapper::Dom::Attribute] attribute
+      # @return [PropertyName]
       def get_attribute_name attribute
         PropertyName.new get_accessor_name(attribute.name), get_var_name(attribute.name)
       end
 
+      # @param [WsdlMapper::Dom::SimpleType] type
+      # @param [WsdlMapper::Dom::EnumerationValue] enum_value
+      # @return [EnumerationValueName]
       def get_enumeration_value_name type, enum_value
         EnumerationValueName.new get_constant_name(enum_value.value), get_key_name(enum_value.value)
       end
-
+      # @param [WsdlMapper::Dom::ComplexType, WsdlMapper::Dom::SimpleType] type
+      # @return [PropertyName]
       def get_content_name type
         @content_name ||= PropertyName.new get_accessor_name(@content_attribute_name), get_var_name(@content_attribute_name)
       end
